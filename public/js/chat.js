44aa6fa6356c95411bc8bf00eb6594bd22a49550
Mauricio Zaparoli <mxavzapa@motorola.com>
2021-06-01 17:07:26 -0300

    Video chat working

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,28 @@
-var socket = io.connect('http://localhost:3000')
-var divVideoChatLobby = document.getElementById('video-chat-lobby')
-var divVideoChat = document.getElementById('video-chat-room')
-var joinButton = document.getElementById('join')
-var userVideo = document.getElementById('user-video')
-var peerVideo = document.getElementById('peer-video')
-var roomInput = document.getElementById('roomName')
-var creator = false
+const socket = io('http://localhost:3000')
+const divVideoChatLobby = document.getElementById('video-chat-lobby')
+const divVideoChat = document.getElementById('video-chat-room')
+const joinButton = document.getElementById('join')
+const userVideo = document.getElementById('user-video')
+const peerVideo = document.getElementById('peer-video')
+const roomInput = document.getElementById('roomName')
 
-var iceServers = [
-  { url: "stun:stun.services.mozilla.com"},
-  { url: "stun1.l.google.com:19302"}
-]
+let userStream
+let creator = false
+
+const stunServers = {
+  iceServers: [
+    { urls: 'stun:stun.services.mozilla.com'},
+    { urls: 'stun:stun.l.google.com:19302'}
+  ]
+}
+const rtcPeerConnection = new RTCPeerConnection(stunServers)
+
 
 const constraints = {
   audio: true,
   video: {
-    width: 1280,
-    height: 720
+    width: 500,
+    height: 500
   }
 }
 
@@ -30,8 +36,10 @@ joinButton.addEventListener('click', () => {
 
 socket.on('created', () => {
   creator = true
-  
+  const divVideoChatLobby = document.getElementById('video-chat-lobby')
+
   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    userStream = stream
     divVideoChatLobby.style = 'display:none'
     userVideo.srcObject = stream
   }).catch((e) => {
@@ -43,6 +51,7 @@ socket.on('joined', () => {
   creator = false
 
   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    userStream = stream
     divVideoChatLobby.style = 'display:none'
     userVideo.srcObject = stream
     socket.emit('ready', roomInput.value)
@@ -55,8 +64,48 @@ socket.on('full', () => {
   alert('Room is full. Cant\'t join')
 })
 
-socket.on('candidate', () => {})
+socket.on('ready', () => {
+  if (creator) {
+    rtcPeerConnection.onicecandidate = OnIceCandidate
+    rtcPeerConnection.ontrack = OnTrack
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream)
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream)
+    rtcPeerConnection.createOffer().then((offer) => {
+      rtcPeerConnection.setLocalDescription(offer)
+      socket.emit('offer', offer, roomInput.value)
+    }).catch((e) => console.log('Error', e))
+  }
+})
 
-socket.on('offer', () => {})
+socket.on('candidate', (candidate) => {
+  let iceCandidate = new RTCIceCandidate(candidate)
+  rtcPeerConnection.addIceCandidate(iceCandidate)
+})
 
-socket.on('answer', () => {})
+socket.on('offer', (offer) => {
+  if (!creator) {
+    rtcPeerConnection.onicecandidate = OnIceCandidate
+    rtcPeerConnection.ontrack = OnTrack
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream)
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream)
+    rtcPeerConnection.setRemoteDescription(offer)
+    rtcPeerConnection.createAnswer().then((answer) => {
+      rtcPeerConnection.setLocalDescription(answer)
+      socket.emit('answer', answer, roomInput.value)
+    }).catch((e) => console.log('Error', e))
+  }
+})
+
+socket.on('answer', (answer) => {
+  rtcPeerConnection.setRemoteDescription(answer)
+})
+
+const OnIceCandidate = (event) => {
+  if (event.candidate) {
+    socket.emit('candidate', event.candidate, roomInput.value)
+  }
+}
+
+const OnTrack = (event) => {
+  peerVideo.srcObject = event.streams[0]
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const socket = require('socket.io')
-var app = express()
+let app = express()
 
 const publicDirectory = path.join(__dirname, '../public/')
 
@@ -11,14 +11,14 @@ let server = app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
 
-var io = socket(server)
+let io = socket(server)
 
 io.on('connection', (socket) => {
   console.log('User connected:' + socket.id)
 
   socket.on('join', (roomName) => {
-    var rooms = io.sockets.adapter.rooms;
-    var room = rooms.get(roomName)
+    let rooms = io.sockets.adapter.rooms;
+    let room = rooms.get(roomName)
 
     if (!room) {
       socket.join(roomName)
@@ -34,22 +34,18 @@ io.on('connection', (socket) => {
   })
 
   socket.on('ready', (roomName) => {
-    console.log('Ready')
-    socket.broadcast.on(roomName).emit('ready')
+    socket.broadcast.to(roomName).emit('ready')
   })
   
   socket.on('candidate', (candidate, roomName) => {
-    console.log('Candidate')
-    socket.broadcast.on(roomName).emit('candidate', candidate)
+    socket.broadcast.to(roomName).emit('candidate', candidate)
   })
   
   socket.on('offer', (offer, roomName) => {
-    console.log('Offer')
-    socket.broadcast.on(roomName).emit('offer', offer)
+    socket.broadcast.to(roomName).emit('offer', offer)
   })
   
   socket.on('answer', (answer, roomName) => {
-    console.log('Answer')
-    socket.broadcast.on(roomName).emit('answer', answer)
+    socket.broadcast.to(roomName).emit('answer', answer)
   })
 })
